6663c4234e6b42207cfed50144ea78ff975057a0
Vítor Alcântara de Almeida <vit.alcantara@gmail.com>
2022-06-03 14:54:13 -0300

    a

--- src/main/java/com/appsdeveloperblog/app/ws/service/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/UserServiceImpl.java
@@ -7,28 +7,35 @@ import org.springframework.stereotype.Service;
 import com.appsdeveloperblog.app.ws.UserRepository;
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+import com.appsdeveloperblog.app.ws.shared.dto.Utils;
 
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserRepository userRepository;
-	
+
+	@Autowired
+	Utils utils;
+
 	@Override
 	public UserDto createUser(UserDto user) {
-		
+
+		if (userRepository.findUserByEmail(user.getEmail()) != null)
+			throw new RuntimeException("Record already exists");
+
 		UserEntity userEntity = new UserEntity();
-		BeanUtils.copyProperties(user,userEntity);
-		
+		BeanUtils.copyProperties(user, userEntity);
+
+		String publicUserId = utils.generateUserId(30);
+		userEntity.setUserId(publicUserId);
 		userEntity.setEncryptedPassword("test");
-		userEntity.setUserId("testUserId");
-		
+
 		UserEntity storedUserDetails = userRepository.save(userEntity);
-		
+
 		UserDto returnValue = new UserDto();
 		BeanUtils.copyProperties(storedUserDetails, returnValue);
-	
-		
+
 		return returnValue;
 	}
 
